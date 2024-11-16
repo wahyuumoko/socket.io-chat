937bf1608e012d3f35c966d578bdea1e2f2267f1
wahyumoko <wahyuprihatmoko788@gmail.com>
2024-11-16 21:01:10 +0700

    pertama chat socket

---
+++ server.js
@@ -0,0 +1,37 @@
+import { createServer } from "node:http";
+import next from "next";
+import { Server } from "socket.io";
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+
+const app = next({ dev, hostname, port });
+const handler = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const httpServer = createServer(handler);
+
+  const io = new Server(httpServer);
+
+  io.on("connection", (socket) => {
+    console.log(socket.id);
+
+    socket.on("message", (msg) => {
+      socket.broadcast.emit("message", msg);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected", socket.id);
+    });
+  });
+
+  httpServer
+    .once("error", (err) => {
+      console.log(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`Ready on http://${hostname}:${port}`);
+    });
+});
---
+++ src/app/utils/socket.js
@@ -0,0 +1,5 @@
+"use client";
+
+import { io } from "socket.io-client";
+
+const socket = io();
